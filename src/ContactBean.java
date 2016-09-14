2186871bc24471c1f05b6d30cdd1ae4f3b9c6912
Cesar A. Garcia <mrcesar9402@gmail.com>
2016-09-14 11:00:10 -0400

    adicion de contacto y modificacion terminado, practica terminada #tentativamente

--- src/ContactBean.java
+++ src/ContactBean.java
@@ -41,8 +41,33 @@ public class ContactBean implements Serializable {
     }
 
     public String guardar() {
-        id = -1;
-        name = apellido = direccion = telefono = correo = "";
+        //si entro al form desde afuera o desde datatable con ref al contacto seleccionado
+        if(id == -1) {
+            Contacto newContacto = new Contacto();
+            newContacto.setNombre(name);
+            newContacto.setApellido(apellido);
+            newContacto.setDireccion(direccion);
+            newContacto.setTelefono(telefono);
+            newContacto.setCorreo(correo);
+
+            contactos.put(newContacto.getId(),newContacto);
+        }
+        else {
+            Contacto target = contactos.get(id);
+            target.setNombre(name);
+            target.setApellido(apellido);
+            target.setDireccion(direccion);
+            target.setTelefono(telefono);
+            target.setCorreo(correo);
+        }
+
+        limpiarDatos();
+
+        return "index";
+    }
+
+    public String volver() {
+        limpiarDatos();
 
         return "index";
     }
@@ -57,10 +82,20 @@ public class ContactBean implements Serializable {
             correo = contactoSeleccionado.getCorreo();
             contactoSeleccionado = null;
 
-            return "welcome";
+            return "form";
+        }
+        else {
+            id = -1;
         }
 
         return null;
+    }
+
+    public void limpiarDatos() {
+
+        id = -1;
+        name = apellido = direccion = telefono = correo = "";
+        contactoSeleccionado = null;
     }
 
     public static ArrayList<Contacto> getContactos() {
